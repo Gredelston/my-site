75f11780649901ebb899035abb9270ac6267052e
Greg Edelston <gredelston@gmail.com>
2018-10-12 11:55:50 -0600

    Dynamically generate PageHandlers based on config.py json

--- app.py
+++ app.py
@@ -8,51 +8,46 @@ import config
 
 class PageHandler(tornado.web.RequestHandler):
     template = "base.html"
-    navbar_current = None
+    navbar_display_text = None
+    navbar_items = [
+        {
+            "display-text": page["navbar-display-text"],
+            "route": page["route"]
+        } for page in config.pages if page["navbar-display"]
+    ]
 
     def get(self):
-        navbar_items = [
-            {
-                "name": "Home",
-                "route": "/"
-            },
-            {
-                "name": "About",
-                "route": "/about"
-            }
-        ]
         self.render(
             self.template,
-            navbar_items=navbar_items,
-            navbar_current=self.navbar_current)
-
-
-class HomepageHandler(PageHandler):
-    template = "home.html"
-    navbar_current = "Home"
-
-
-class AboutPageHandler(PageHandler):
-    template = "about.html"
-    navbar_current = "About"
+            navbar_items=self.navbar_items,
+            navbar_current=self.navbar_display_text)
 
 
 ### Application logic
 
 def make_app():
-    handlers = [
-        (r"/", HomepageHandler),
-        (r"/home", HomepageHandler),
-        (r"/index", HomepageHandler),
-        (r"/about", AboutPageHandler)
-    ]
+    handlers = create_route_handlers(config.pages)
     settings = {
         "static_path": config.static_path,
         "template_path": config.template_path,
         "debug": True
-
     }
     return tornado.web.Application(handlers, **settings)
+
+
+def create_route_handlers(pages):
+    handlers = []
+    for page in pages:
+        route = page["route"]
+        handler_name = page["name"] + "Handler"
+        handler_attributes = {
+            "template": page["template"],
+            "navbar_display_text": page.get("navbar-display-text", None)
+        }
+        page_handler = type(handler_name, (PageHandler, ), handler_attributes)
+        handlers.append((route, page_handler))
+    return handlers
+
 
 def shutdown_app():
     print("Shutting down app.")
--- config.py
+++ config.py
@@ -5,3 +5,22 @@ port = 8888
 base_dir = os.path.dirname(__file__)
 static_path = os.path.join(base_dir, "static")
 template_path = os.path.join(base_dir, "template")
+
+pages = [
+    {
+        "name": "Home",
+        "route": "/",
+        "template": "home.html",
+        "navbar-display": True,
+        "navbar-display-text": "Home",
+        "navbar-children": None
+    },
+    {
+        "name": "About",
+        "route": "/about",
+        "template": "about.html",
+        "navbar-display": True,
+        "navbar-display-text": "About Me",
+        "navbar-children": None
+    }
+]
